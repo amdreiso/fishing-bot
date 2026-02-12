bcf47b75177a7495b6a1892ea1f208cc63befe7f
amdreiso <andreiscatolin@gmail.com>
2026-02-12 20:39:02 -0300

    remove redundancy

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import pyautogui as p
 RUNNING = True
 TIME = 0
 
+# R G B | 0 - 255
 color = (0, 0, 0)
+
+fishy = 0
 
 while RUNNING:
     x, y = p.position()
-    print(x, y)
-    width = 1
     height = 100
-    xoff = x - width / 2
     yoff = y - height / 2
-    for i in range(width):
-        for j in range(height):
-            if p.pixelMatchesColor(int(xoff + i), int(yoff + j), color):
-                print("found fish")
-                p.dragTo(x, y, 1)
+    for i in range(height):
+        if p.pixelMatchesColor(x, int(yoff + i), color):
+            fishy += 1 
+            print("found fish: ", fishy)
+            p.dragTo(x, y, 1)
 
 
